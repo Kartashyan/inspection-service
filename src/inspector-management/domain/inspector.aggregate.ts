8c3ece3f59150460acbec91433d14aa0c80b960a
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-30 21:19:06 +0200

    feat: Add assignInspection method to Inspector aggregate class

--- src/inspector-management/domain/inspector.aggregate.ts
+++ src/inspector-management/domain/inspector.aggregate.ts
@@ -1,16 +1,23 @@
 import { AggregateRoot } from "src/core-tools/aggregate-root";
 import { UID } from "src/core-tools/id";
+import { SubscriptionLevel } from "./subscription-level";
 
 type InspectorProps = {
     id: UID;
     schedule: {
         date: Date;
         inspectionId: UID;
+        inspectionLevel: SubscriptionLevel;
     }[];
+    subscriptionLevelsAllowed: SubscriptionLevel[];
 };
 
 export class Inspector extends AggregateRoot<InspectorProps> {
     constructor(props: InspectorProps) {
         super(props);
+    }
+
+    assignInspection(date: Date, inspectionId: UID, inspectionLevel: SubscriptionLevel): void {
+        this.props.schedule.push({ date, inspectionId, inspectionLevel });
     }
 }
