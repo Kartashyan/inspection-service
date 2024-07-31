1b7f33b32ff44e5f251d68d53abf930bf2d1c039
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-31 23:50:45 +0200

    feat: Add InspectionDate value object for managing inspection dates

--- src/inspector-management/domain/inspection.aggregate.ts
+++ src/inspector-management/domain/inspection.aggregate.ts
@@ -1,11 +1,12 @@
 import { AggregateRoot } from "src/core-tools/aggregate-root";
 import { UID } from "src/core-tools/id";
 import { SubscriptionLevel } from "./subscription-level";
+import { InspectionDate } from "./inspection-date.value-object";
 
 export interface InspectionProps {
     id: UID;
-    inspectorId: string;
-    inspectionDate: Date;
+    inspectorId: UID;
+    inspectionDate: InspectionDate;
     subscriptionLevel: SubscriptionLevel;
 }
 
