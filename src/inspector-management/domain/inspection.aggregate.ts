8f2862d2c72615e53b4593029bd8be7a0db096cb
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-01 20:44:38 +0200

    feat: Add InspectionCreatedEvent to Inspection aggregate class

--- src/inspector-management/domain/inspection.aggregate.ts
+++ src/inspector-management/domain/inspection.aggregate.ts
@@ -2,6 +2,7 @@ import { AggregateRoot } from "src/core-tools/aggregate-root";
 import { UID } from "src/core-tools/id";
 import { SubscriptionLevel } from "./subscription-level";
 import { InspectionDate } from "./inspection-date.value-object";
+import { InspectionCreatedEvent } from "./domain-events/inspection-created.event";
 
 export interface InspectionProps {
     id: UID;
@@ -13,5 +14,13 @@ export interface InspectionProps {
 export class Inspection extends AggregateRoot<InspectionProps> {
     constructor(props: InspectionProps) {
         super(props);
+    }
+
+    static create(props: InspectionProps): Inspection {
+        const inspection = new Inspection(props);
+        if(props.id.isNew) {
+            inspection.addDomainEvent(new InspectionCreatedEvent(inspection));
+        }
+        return inspection;
     }
 }
