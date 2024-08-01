8b74d055c022d2be78cfdbf06d485ddc799bf133
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-01 22:16:26 +0200

    feat: Implement HandleNewInspectionRequestUseCase for handling new inspection requests

---
+++ src/inspector-management/applicaation/handle-new-inspection-request.use-csae.ts
@@ -0,0 +1,27 @@
+import { UID } from "src/core-tools/id";
+import { InspectionDate } from "../domain/inspection-date.value-object";
+import { Inspection, InspectionProps } from "../domain/inspection.aggregate";
+import { ClientsRepositoryPort } from "../domain/ports/client-repository.port";
+import { InspectionsRepositoryPort } from "../domain/ports/inspections-repository.port";
+import { InspectionRequestDto } from "./inspection.dto";
+
+export class HandleNewInspectionRequestUseCase {
+    constructor(
+        private inspectionsRepository: InspectionsRepositoryPort,
+        private clientsRepository: ClientsRepositoryPort
+    ) {}
+
+    async handle(request: InspectionRequestDto): Promise<void> {
+        const client = await this.clientsRepository.findById(request.clientId);
+        const inspectionProps: InspectionProps<false> = {
+            id: new UID(),
+            inspectorId: null,
+            requestedDate: new InspectionDate(new Date()),
+            inspectionDate: null,
+            subscriptionLevel: client.getSubscriptionLevel(),
+            isScheduled: false
+        };
+        const inspection = Inspection.create(inspectionProps);
+        await this.inspectionsRepository.save(inspection);
+    }
+}
