91edc50dc18c370a082dac252071b49c280d303c
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-02 23:43:53 +0200

    refactor: Remove unused methods from InspectionsRepositoryPort interface

--- src/inspector-management/domain/ports/inspections-repository.port.ts
+++ src/inspector-management/domain/ports/inspections-repository.port.ts
@@ -4,8 +4,8 @@ import { SubscriptionLevel } from "../subscription-level";
 export interface InspectionsRepositoryPort {
     save(inspection: Inspection): Promise<void>;
     findById(inspectionId: string): Promise<Inspection>;
-    findByInspectorId(inspectorId: string): Promise<Inspection[]>;
-    findByClientId(clientId: string): Promise<Inspection[]>;
-    findBySiteId(siteId: string): Promise<Inspection[]>;
-    findBySubscriptionLevel(subscriptionLevel: SubscriptionLevel): Promise<Inspection[]>;
+    // findByInspectorId(inspectorId: string): Promise<Inspection[]>;
+    // findByClientId(clientId: string): Promise<Inspection[]>;
+    // findBySiteId(siteId: string): Promise<Inspection[]>;
+    // findBySubscriptionLevel(subscriptionLevel: SubscriptionLevel): Promise<Inspection[]>;
 }
