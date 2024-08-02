2e317a3b2e32a84574ebd5781eb85cf57026b89b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-02 23:44:08 +0200

    feat: Add InspectionInMemoryTestRepositoryAdapter for managing inspection repository

---
+++ src/inspector-management/infra/inspection-repository.adapter.ts
@@ -0,0 +1,17 @@
+import { UID } from "src/core-tools/id";
+import { Inspection } from "../domain/inspection.aggregate";
+import { InspectionsRepositoryPort } from "../domain/ports/inspections-repository.port";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+
+export class InspectionInMemoryTestRepositoryAdapter implements InspectionsRepositoryPort {
+    private inspections: Inspection[] = [];
+
+    async save(inspection: Inspection): Promise<void> {
+        this.inspections.push(inspection);
+        inspection.publishEvents(new EventEmitter2());
+    }
+
+    async findById(inspectionId: string): Promise<Inspection> {
+        return this.inspections.find(inspection => inspection.id.equals(new UID(inspectionId)));
+    }
+}
