34750c5fcb4dec76771cc26121f8508a08f47fa0
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-06 23:24:22 +0200

    refactor: Add InspectionSchedulingDomainService for managing inspection scheduling

---
+++ src/inspector-management/domain/inspection-scheduling.domain-service.ts
@@ -0,0 +1,19 @@
+import { Inspection } from "./inspection.aggregate";
+import { InspectorRepositoryPort } from "./ports/inspector-repository.port";
+
+export class InspectionSchedulingDomainService {
+    constructor(private inspectorRepository: InspectorRepositoryPort) {
+        this.inspectorRepository = inspectorRepository;
+    }
+
+    async schedule(inspection: Inspection): Promise<void> {
+        const inspectors = await this.inspectorRepository.findBySubscriptionLevel(inspection.getSubscriptionLevel());
+
+        const leastBusyInspector = inspectors.reduce((prev, current) => {
+            return prev.getSchedule().length < current.getSchedule().length ? prev : current;
+        });
+
+        leastBusyInspector.assignInspection(inspection.getRequestedDate(), inspection.id, inspection.getSubscriptionLevel());
+        await this.inspectorRepository.save(leastBusyInspector);
+    }
+}
