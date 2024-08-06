68eecd255d63d09f9940958699cb35b8564eb6c4
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-06 23:21:19 +0200

    refactor: Update DomainEvent class to include aggregateId in constructor

--- src/core-tools/domain-event.ts
+++ src/core-tools/domain-event.ts
@@ -1,5 +1,14 @@
-export interface DomainEvent<T> {
-    name: string;
-    occuredAt: Date;
-    aggregate: T;
+type EventProps = {
+    aggregateId: string;
+}
+
+export abstract class DomainEvent {
+    readonly occuredAt: Date;
+    readonly name: string;
+    readonly aggregateId: string;
+
+    constructor(readonly props: EventProps) {
+        this.occuredAt = new Date();
+        this.aggregateId = props.aggregateId;
+    }
 }
