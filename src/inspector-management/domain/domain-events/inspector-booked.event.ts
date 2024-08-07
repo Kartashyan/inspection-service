8af41a5c04a71969d01a172114de861a967b7c50
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-07 23:31:55 +0200

    refactor: Update InspectorBookedEvent constructor to include inspectorId and inspectionDate

--- src/inspector-management/domain/domain-events/inspector-booked.event.ts
+++ src/inspector-management/domain/domain-events/inspector-booked.event.ts
@@ -1,20 +1,34 @@
 import { DomainEvent } from "src/core-tools/domain-event";
 import { Inspector } from "../inspector.aggregate";
 import { AggregateRoot } from "src/core-tools/aggregate-root";
+import { inspect } from "util";
+import { InspectionDate } from "../inspection-date.value-object";
+import { UID } from "src/core-tools/id";
+import { SubscriptionLevel } from "../subscription-level";
+
+type InspectionDto = {
+    date: InspectionDate;
+    inspectionId: UID;
+    inspectionLevel: SubscriptionLevel;
+}
 
 export class InspectorBookedEvent extends DomainEvent {
     readonly occuredAt: Date;
     readonly name = "inspector-booked";
+    readonly inspectorId: UID;
+    readonly inspectionDate: InspectionDate;
 
-    constructor(readonly aggregate: Inspector) {
+    constructor(readonly aggregateId: UID, inspectionDto: InspectionDto) {
         super({
-            aggregateId: aggregate.id.value,
+            aggregateId: aggregateId,
         });
         this.occuredAt = new Date();
+        this.inspectorId = aggregateId;
+        this.inspectionDate = inspectionDto.date
     }
 
-    getAggregateId(): string {
-        return this.aggregate.id.value;
+    getAggregateId(): UID {
+        return this.inspectorId;
     }
 
 }
