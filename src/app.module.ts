06ff4ba84b81a2f5a7384f711b9811b4fd8133af
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-09 18:25:25 +0200

    feat: Add InspectionManagementModule for managing inspections

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InspectorManagementModule } from './inspector-management/inspector-management.module';
+import { InspectionManagementModule } from './inspection-management/inspection-management.module';
 
 @Module({
-  imports: [InspectorManagementModule],
+  imports: [InspectorManagementModule, InspectionManagementModule],
   controllers: [AppController],
   providers: [AppService],
 })
