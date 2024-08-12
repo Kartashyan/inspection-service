9ea30e2c4dc6f29a51d2a681e135aa6acb92bae9
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-12 20:34:51 +0200

    feat: Add class-validator and IsString decorator to inspection request DTO

--- src/inspection-management/infra/register-inspection-request.dto.ts
+++ src/inspection-management/infra/register-inspection-request.dto.ts
@@ -1,10 +1,12 @@
-import { MaxLength, MinLength } from "class-validator";
+import { IsString, MaxLength, MinLength } from "class-validator";
 
 export class RegisterInspectionRequestDto {
     @MaxLength(320)
     @MinLength(5)
+    @IsString()
     readonly clientId: string;
     @MaxLength(320)
     @MinLength(5)
+    @IsString()
     readonly siteId: string;
 }
