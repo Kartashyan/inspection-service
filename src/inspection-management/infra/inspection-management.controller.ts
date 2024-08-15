061ee5436102483d3353e1fc247e1dd7a6317037
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-08-15 18:16:07 +0200

    refactor: Handle ClientNotFoundError in InspectionManagementController

--- src/inspection-management/infra/inspection-management.controller.ts
+++ src/inspection-management/infra/inspection-management.controller.ts
@@ -1,15 +1,25 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
-import { RegisterInspectionRequestDto } from './register-inspection-request.dto';
-import { RegisterInspectionCommand } from '../applicaation/inspection/register-inspection.command';
+import { Body, Controller, NotFoundException, Post, ValidationPipe } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { ClientNotFoundError } from '../applicaation/client-not-found.error';
+import { RegisterInspectionCommand } from '../applicaation/inspection/register-inspection.command';
+import { RegisterInspectionRequestDto } from './register-inspection-request.dto';
 
 @Controller('inspection-management')
 export class InspectionManagementController {
-    constructor(private readonly commandBus: CommandBus) {}
+    constructor(private readonly commandBus: CommandBus) { }
     @Post('register-new')
     async createInspection(@Body(ValidationPipe) body: RegisterInspectionRequestDto): Promise<string> {
         const command = new RegisterInspectionCommand(body.clientId, body.siteId);
-        await this.commandBus.execute(command);
-        return `New inspection registered for client ${command.clientId} and site ${command.siteId}`;
+        try {
+            await this.commandBus.execute(command);
+            return `New inspection registered for client ${command.clientId} and site ${command.siteId}`;
+        } catch (error) {
+            if (error instanceof ClientNotFoundError) {
+                throw new NotFoundException(error.message);
+            } else {
+                throw error;
+            }
+
+        }
     }
 }
